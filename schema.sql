16f789ea3dfa40d9bd8b9cb7907786a2f8df3a62
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-24 15:21:32 -0700

    Created an employee.js file with template code from reference activity.  Input table data into seeds.sql file.

--- schema.sql
+++ schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE employee (
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   role_id INTEGER NOT NULL,
-  manager_id INTEGER NULL
+  manager_id INTEGER NULL,
   PRIMARY KEY (id)
 );
 
@@ -22,23 +22,8 @@ CREATE TABLE role (
   id INTEGER NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL(10, 2) NOT NULL,
-  department_id INTEGER
-  PRIMARY KEY (id),
-  -- UNIQUE constraint can make a column, or multiple
-  -- columns combined only able to have one match that is the same.
-  -- in this case there could be a matching username only if
-  -- it is in a different group
-  UNIQUE(username, user_group_id),
-  -- INDEX makes a certain column faster to do joins on
-  -- in this case we make `user_group_id` faster to join on
-  INDEX user_group_ind(user_group_id),
-  -- FOREIGN KEY sets up a column as a reference to a
-  -- column in another table
-  -- in this case we say user_group_id refers to table `user_group`'s
-  -- `id` column
-  FOREIGN KEY (user_group_id)
-	REFERENCES user_group(id)
-    ON DELETE CASCADE
+  department_id INTEGER,
+  PRIMARY KEY (id)
 );
 
 
@@ -49,25 +34,5 @@ CREATE TABLE role (
 CREATE TABLE department (
   id INTEGER NOT NULL AUTO_INCREMENT,
   name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id),
-  -- INDEX makes a certain column faster to do joins on
-  -- in this case we make `user_id` faster to join on
-  INDEX user_ind(user_id),
-  -- INDEX makes a certain column faster to do joins on
-  -- in this case we make `responding_to_id` faster to join on
-  INDEX responding_to_ind(responding_to_id),
-  -- FOREIGN KEY sets up a column as a reference to a
-  -- column in another table
-  -- in this case we say user_id refers to table `user`'s
-  -- `id` column
-  FOREIGN KEY (user_id)
-	REFERENCES user(id)
-    ON DELETE CASCADE,
-  -- FOREIGN KEY sets up a column as a reference to a
-  -- column in another table (or in this case itself)
-  -- in this case we say responding_to_id refers to table `comment`'s
-  -- `id` column
-  FOREIGN KEY (responding_to_id)
-	REFERENCES comment(id)
-    ON DELETE CASCADE
+  PRIMARY KEY (id)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,47 +1,28 @@
 USE employee_trackerDB;
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES (),
-        (),
-        (),
-        (),
-        ();
-
-INSERT INTO role (title, salary, department_id)
-VALUES (), -- Umeyarus is in Hallowed Oblivion group
-        (), -- Umeyarus is in Hallowed Oblivion group
-        (), -- Umeyarus is in Hallowed Oblivion group
-        (), -- Umeyarus is in Hallowed Oblivion group
+VALUES ("Kris", "Chamberlin", 3, 1),
+        ("Aleah", "Gonsalves", 1, 2),
+        ("Daisy", "Dog", 4, 3),
+        ("Mingas", "Galinas", 6, 4),
+        ("Amber", "Goff", 8, 4),
+        ("Jarrod", "Gaut", 5, 5),
+        ("Tessa", "Overfelt", 2, 6),
+        ("Sean", "Emery", 7, 7);
 
 INSERT INTO department (name)
-VALUES (), -- Umeyarus is in Hallowed Oblivion group
-        (), -- Umeyarus is in Hallowed Oblivion group
-        (), -- Umeyarus is in Hallowed Oblivion group
+VALUES ("Sales"), 
+        ("Legal"), 
+        ("Engineering"), 
+        ("Finance");
 
 
-
-
-
-
-
-
-
-INSERT INTO comment (id, text, user_id) -- this will be starting comments
-VALUES (1, "Discussion Thread: Hallowed Oblivion", 1), -- Kearen's comment
-(2, "Discussion Thread: Sacred Ancients", 5), -- Balzeiros's comment
-(3, "Discussion Thread: Hammers of the Fox", 8), -- Adgolor's comment
-(4, "Discussion Thread: Division of the Sacred", 12), -- Xyrzana's comment
-(5, "Garage Sale", 9), -- Naesalor's comment
-(6, "Discussion Thread: Forge Hawks", 14), -- Sylkrana's comment
-(7, "I love this database", 1); -- Kearen's comment
-
-INSERT INTO comment (text, user_id, responding_to_id) -- these will be responses
-VALUES ("We rule!", 2, 1), -- Ilinorin's response to comment id 1
-("We rock!", 6, 2), -- Heiris's response to comment id 2
-("We are the best!", 11, 3), -- Quira's response to comment id 3
-("We are awesome!", 13, 4), -- Bryna's response to comment id 4
-("I am ready to buy!", 2, 5), -- Ilinorin's response to comment id 5
-("We are amazing!", 18, 6), -- Gregwyn's response to comment id 6
-("Me too!", 2, 7), -- Ilinorin's response to comment id 7
-("No you don't!", 10, 8), -- Liajyre's response to comment id 8 (which itself is a response!)
-("WOOOOO!", 7, 2); -- Omaydark's response to comment id 2
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 90000, 1), 
+        ("Salesperson", 50000, 1), 
+        ("Lawyer", 100000, 2),
+        ("Paralegal", 60000, 2), 
+        ("Lead Engineer", 100000, 3),
+        ("Software Engineer", 50000, 3),
+        ("Lead Accountant", 75000, 4),
+        ("Bookkeeper", 25000, 4);
