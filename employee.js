a85fbc81a0e6de79c91fedf5e837c472934ab47f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-24 17:56:08 -0700

    Inserted correct starting code for employee.js file, including required modules and connections.  Made a start function that uses inquirer to prompt the user what they would like to do in NodeJS with options 'view all employees, roles, and departments'.  Created functions for viewing all employees and roles

--- employee.js
+++ employee.js
@@ -3,44 +3,86 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "",
-  database: "greatBay_DB"
+    // Your password
+    password: "root",
+    database: "employee_trackerDB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
+connection.connect(function (err) {
+    if (err) throw err;
+    // run the start function after the connection is made to prompt the user
+    start();
 });
+
 
 // function which prompts the user for what action they should take
 function start() {
-  inquirer
-    .prompt({
-      name: "postOrBid",
-      type: "list",
-      message: "Would you like to [POST] an auction or [BID] on an auction?",
-      choices: ["POST", "BID", "EXIT"]
+    inquirer
+        .prompt({
+            name: "whatDo",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Roles",
+                "View All Departments",
+                "EXIT"]
+        })
+        .then(function (answer) {
+            if (answer.whatDo === "View All Employees") {
+                console.log("view all employees");
+                viewEmployees();
+            }
+            else if (answer.whatDo === "View All Roles") {
+                console.log("view all roles");
+                viewRoles();
+            // }
+            
+
+
+
+
+            } else {
+                connection.end();
+            }
+        });
+}
+
+
+
+// ==============================
+//         MAIN FUNCTIONS
+// ==============================
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//        View All Employees
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function viewEmployees() {
+    connection.query("SELECT * FROM employee", function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
     })
-    .then(function(answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.postOrBid === "POST") {
-        postAuction();
-      }
-      else if(answer.postOrBid === "BID") {
-        bidAuction();
-      } else{
-        connection.end();
-      }
-    });
+}
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//        View All Roles
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function viewRoles() {
+    connection.query("SELECT * FROM role", function (err, results) {
+        if (err) throw err;
+        console.table(results);
+        start();
+    })
 }
